11f05bb588ba6cc3eb6cd18e895205f41e4b056c
HaikalFadhilah <110004043+haikalfadhilah05@users.noreply.github.com>
2022-09-17 16:09:45 +0700

    Delete input-config.php

--- input-config.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $mysqli = new mysqli("localhost","root","","siswa_rpl");
-    if ($mysqli -> connect_errno) {
-        echo "failed to connect to mySQL: " . $mysqli -> connect_error;
-        exit();
-    }
-?>
